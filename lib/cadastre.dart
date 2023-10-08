5b277c8c57a0abd0f8e7147dfecb5d4916d10b09
Kaique Silva <90569908+kaiquewolf@users.noreply.github.com>
2023-10-08 13:10:45 -0300

    Pagina de cadastro estática terminada

--- lib/cadastre.dart
+++ lib/cadastre.dart
@@ -10,6 +10,34 @@ class CadastrePage extends StatefulWidget {
 }
 
 class _CadastrePageState extends State<CadastrePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xfff2f0f0),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 100.0),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: const Column(
+            children: <Widget>[
+              CadastrePageForm(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CadastrePageForm extends StatefulWidget {
+  const CadastrePageForm({super.key});
+
+  @override
+  State<CadastrePageForm> createState() => _CadastrePageFormState();
+}
+
+class _CadastrePageFormState extends State<CadastrePageForm> {
   final _formKey = GlobalKey<FormState>();
   final nameController = TextEditingController();
   final emailController = TextEditingController();
@@ -20,20 +48,26 @@ class _CadastrePageState extends State<CadastrePage> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(
-        top: 20,
+        top: 50,
+        bottom: 10,
         left: 20,
-        right: 20,
-        bottom: 20,
+        right: 30,
       ),
       child: Form(
-        // key: _formKey,
+        key: _formKey,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Padding(
-              padding: EdgeInsets.all(300),
-              child: Text("Nome"),
+              padding: EdgeInsets.only(right: 300, bottom: 10),
+              child: Text(
+                "Nome",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
             ),
             TextFormField(
               keyboardType: TextInputType.name,
@@ -53,8 +87,14 @@ class _CadastrePageState extends State<CadastrePage> {
               height: 20,
             ),
             const Padding(
-              padding: EdgeInsets.all(300),
-              child: Text("Email"),
+              padding: EdgeInsets.only(right: 300, bottom: 10),
+              child: Text(
+                "Email",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
             ),
             TextFormField(
               keyboardType: TextInputType.emailAddress,
@@ -76,8 +116,14 @@ class _CadastrePageState extends State<CadastrePage> {
               height: 20,
             ),
             const Padding(
-              padding: EdgeInsets.all(300),
-              child: Text("Senha"),
+              padding: EdgeInsets.only(right: 300, bottom: 10),
+              child: Text(
+                "Senha",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
             ),
             TextFormField(
               keyboardType: TextInputType.visiblePassword,
@@ -98,19 +144,59 @@ class _CadastrePageState extends State<CadastrePage> {
             const SizedBox(
               height: 20,
             ),
-            Switch(
-              value: isSwitched,
-              onChanged: (value) {
-                setState(
-                  () async {
-                    isSwitched = value;
-                    if (isSwitched == true) {
-                      await Geolocator.requestPermission();
-                    }
-                  },
-                );
+            Row(
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: Switch(
+                    value: isSwitched,
+                    onChanged: (bool value) {
+                      setState(
+                        () {
+                          isSwitched = value;
+                          if (isSwitched == true) {
+                            // Geolocator.requestPermission();
+                          }
+                        },
+                      );
+                    },
+                  ),
+                ),
+                const Text(
+                  "Permitir o acesso da minha \n localização durante o uso do \n aplicativo.",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 50),
+            InkWell(
+              onTap: () {
+                if (_formKey.currentState!.validate()) {
+                  print("Sucesso");
+                  emailController.clear();
+                  passwordController.clear();
+                }
               },
-            )
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: const Color(0xff142949),
+                ),
+                child: const Center(
+                  child: Text(
+                    "Cadastrar",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
